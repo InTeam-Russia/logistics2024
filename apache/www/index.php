0b968a4e5ed689cd794477ff1b318344e0d6c6de
danilasar <danila.sar@yandex.ru>
2024-04-08 11:59:39 +0400

    Создан интимнет

--- apache/www/index.php
+++ apache/www/index.php
@@ -1,5 +1,13 @@
 <?php
 require_once 'vendor/autoload.php';
+spl_autoload_register(function ($class) {
+    $file = str_replace('\\', DIRECTORY_SEPARATOR, $class).'.php';
+    if (file_exists($file)) {
+        require $file;
+        return true;
+    }
+    return false;
+});
 
 use Pecee\SimpleRouter\SimpleRouter;
 
@@ -12,6 +20,9 @@ require_once 'routes.php';
  */
 
 SimpleRouter::setDefaultNamespace('\Demo\Controllers');
+
+//http_send_content_type("text/json");
+header("Content-Type: text/json");
 
 // Start the routing
 SimpleRouter::start();
